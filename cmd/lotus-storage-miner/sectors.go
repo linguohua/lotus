fbdd764fbcf5e09ca5730f6c7c095b560ee79687
linguohua <lghchinaidea@gmail.com>
2021-05-18 08:12:26 +0800

    storage and work group

--- cmd/lotus-storage-miner/sectors.go
+++ cmd/lotus-storage-miner/sectors.go
@@ -59,6 +59,7 @@ var sectorsPledgeCmd = &cli.Command{
 		defer closer()
 		ctx := lcli.ReqContext(cctx)
 
+		// lingh: pledge beginning
 		id, err := nodeApi.PledgeSector(ctx)
 		if err != nil {
 			return err
