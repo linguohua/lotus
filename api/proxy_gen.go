6fc3c174b3684fd80ce9ff30af4e42bd6c927c3d
linguohua <lghchinaidea@gmail.com>
2021-05-18 11:52:36 +0800

    unkonwn error

--- api/proxy_gen.go
+++ api/proxy_gen.go
@@ -20,7 +20,7 @@ import (
 	"github.com/filecoin-project/go-state-types/dline"
 	apitypes "github.com/filecoin-project/lotus/api/types"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/market"
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
