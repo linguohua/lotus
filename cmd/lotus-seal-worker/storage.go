f8f1c477862afb69da2ff0c56d8eda9e8f80018e
linguohua <lghchinaidea@gmail.com>
2021-05-21 20:42:09 +0800

    group id init storage

--- cmd/lotus-seal-worker/storage.go
+++ cmd/lotus-seal-worker/storage.go
@@ -51,6 +51,10 @@ var storageAttachCmd = &cli.Command{
 			Name:  "max-storage",
 			Usage: "(for init) limit storage space for sectors (expensive for very large paths!)",
 		},
+		&cli.StringFlag{
+			Name:  "group",
+			Usage: "group id",
+		},
 	},
 	Action: func(cctx *cli.Context) error {
 		nodeApi, closer, err := lcli.GetWorkerAPI(cctx)
@@ -92,12 +96,15 @@ var storageAttachCmd = &cli.Command{
 				}
 			}
 
+			groupID := cctx.String("group")
+
 			cfg := &stores.LocalStorageMeta{
 				ID:         stores.ID(uuid.New().String()),
 				Weight:     cctx.Uint64("weight"),
 				CanSeal:    cctx.Bool("seal"),
 				CanStore:   cctx.Bool("store"),
 				MaxStorage: uint64(maxStor),
+				GroupID:    groupID,
 			}
 
 			if !(cfg.CanStore || cfg.CanSeal) {
