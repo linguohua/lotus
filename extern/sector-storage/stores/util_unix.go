50c60a7384b4f329d88651acab81c209d2ce8bcc
linguohua <lghchinaidea@gmail.com>
2021-06-05 04:55:23 +0800

    use cp to move sector

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -2,6 +2,9 @@ package stores
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
@@ -27,19 +30,81 @@ func move(from, to string) error {
 
 	log.Debugw("move sector data", "from", from, "to", to)
 
+	// verify if the file exist in target dir
+	fifrom, err := os.Stat(from)
+	if err != nil {
+		log.Errorf("move sector: os.Stat(%s) failed:%s ----> %s, error:%v", from, err)
+		return xerrors.Errorf("os.Stat(%s): %w", from, err)
+	}
+
 	toDir := filepath.Dir(to)
 
 	// `mv` has decades of experience in moving files quickly; don't pretend we
 	//  can do better
 
 	var errOut bytes.Buffer
-	log.Infof("util_unix.call /usr/bin/env mv bein:%s ----> %s", from, to)
-	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
+	log.Infof("util_unix.call /usr/bin/env cp bein:%s ----> %s", from, to)
+	cmd := exec.Command("/usr/bin/env", "cp", "-r", "-t", toDir, from) // nolint
 	cmd.Stderr = &errOut
 	if err := cmd.Run(); err != nil {
-		log.Infof("util_unix.call /usr/bin/env mv failed:%s ----> %s, error:%v", from, to, err)
-		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
+		log.Errorf("util_unix.call /usr/bin/env cp failed:%s ----> %s, error:%v", from, to, err)
+		return xerrors.Errorf("exec cp (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
 	}
-	log.Infof("util_unix.call /usr/bin/env mv end:%s ----> %s", from, to)
+	log.Infof("util_unix.call /usr/bin/env cp end:%s ----> %s, begin verify...", from, to)
+
+	fito, err := os.Stat(to)
+	if err != nil {
+		log.Errorf("move sector: os.Stat(%s) , error:%v", to, err)
+		return xerrors.Errorf("os.Stat(%s): %w", to, err)
+	}
+
+	if !fifrom.IsDir() {
+		if fifrom.Size() != fito.Size() {
+			msg := fmt.Sprintf("move sector: os.Stat size not match, from [%s] %d != to [%s] %d",
+				from, fifrom.Size(), to, fito.Size())
+			log.Errorf(msg)
+			return xerrors.Errorf(msg)
+		}
+	} else {
+		// dir, enumerate each source file
+		files, err := ioutil.ReadDir(from)
+		if err != nil {
+			msg := fmt.Sprintf("move sector: ioutil.ReadDir(%s) failed,error:%v", from, err)
+			log.Errorf(msg)
+			return xerrors.Errorf(msg)
+		}
+
+		for _, f := range files {
+			// only check level 1 files
+			if f.IsDir() {
+				continue
+			}
+
+			dstfn := filepath.Join(to, f.Name())
+			dstfi, err := os.Stat(dstfn)
+			if err != nil {
+				log.Errorf("move sector: os.Stat(%s) , error:%v", dstfn, err)
+				return xerrors.Errorf("move sector: os.Stat(%s): %w", dstfn, err)
+			}
+
+			if f.Size() != dstfi.Size() {
+				srcfn := filepath.Join(from, f.Name())
+				msg := fmt.Sprintf("move sector: os.Stat size not match, from [%s] %d != to [%s] %d",
+					srcfn, f.Size(), dstfn, dstfi.Size())
+				log.Errorf(msg)
+				return xerrors.Errorf(msg)
+			}
+		}
+	}
+
+	log.Infof("move sector: %s ----> %s, verify ok, now call rm -rf to remove %s", from, to, from)
+
+	// remove
+	cmd = exec.Command("/usr/bin/env", "rm", "-rf", from) // nolint
+	cmd.Stderr = &errOut
+	if err := cmd.Run(); err != nil {
+		log.Infof("util_unix.call /usr/bin/env rm failed:%s ----> %s, error:%v", from, to, err)
+	}
+
 	return nil
 }
