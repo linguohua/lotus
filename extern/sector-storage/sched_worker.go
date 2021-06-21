eedfe41eb17932eb6d9f4dc048a6bf1f2941f6b2
linguohua <lghchinaidea@163.com>
2021-06-21 18:42:42 +0800

    log out worker url

--- extern/sector-storage/sched_worker.go
+++ extern/sector-storage/sched_worker.go
@@ -317,7 +317,7 @@ func (sw *schedWorker) checkSession(ctx context.Context) bool {
 }
 
 func (sw *schedWorker) requestWindowsByTasktype(taskType sealtasks.TaskType, i int) bool {
-	log.Infof("schedWorker.requestWindowsByTasktype task type:%s, window count:%d", taskType, i)
+	log.Infof("schedWorker.requestWindowsByTasktype task type:%s, window count:%d, worker url:%s", taskType, i, sw.worker.url)
 	for idx := 0; idx < i; idx++ {
 		select {
 		case sw.sched.windowRequests <- &schedWindowRequest{
