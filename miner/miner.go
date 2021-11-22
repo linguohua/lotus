bebe3560ae2a2633c555e3d4fb2fd73dcfe1a6be
linguohua <lghchinaidea@163.com>
2021-11-22 23:26:07 +0800

    test: old ticket

--- miner/miner.go
+++ miner/miner.go
@@ -672,14 +672,14 @@ func (m *Miner) mineOne(ctx context.Context, base *MiningBase) (minedBlock *type
 		log.Errorf("mineOne GetBestMiningCandidate error:%v", err)
 	}
 
-	if replaceBase {
-		log.Warnf("rebase, re-compute ticket")
-		ticket, err = m.computeTicket(ctx, &rbase, base, mbi)
-		if err != nil {
-			err = xerrors.Errorf("scratching ticket failed for rebase: %w", err)
-			return nil, err
-		}
-	}
+	// if replaceBase {
+	// 	log.Warnf("rebase, re-compute ticket")
+	// 	ticket, err = m.computeTicket(ctx, &rbase, base, mbi)
+	// 	if err != nil {
+	// 		err = xerrors.Errorf("scratching ticket failed for rebase: %w", err)
+	// 		return nil, err
+	// 	}
+	// }
 
 	tHardReplace := build.Clock.Now()
 
